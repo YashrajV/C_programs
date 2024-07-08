697b36345bdc65ebe6ed4d07513526ac18329a3b
YashrajV <yashrajv@vayavyalabs.com>
2024-07-08 12:43:49 +0530

    initial commit

---
+++ test.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s[10]="string";
+    char c='c';
+    printf("hello world\n");
+    printf("%c\n",c);
+    printf("%s",s);
+    return 0;
+
+}
